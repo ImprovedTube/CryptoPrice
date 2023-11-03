bb983dde9eb9019d67eab7917cdfc1dbdce73175
mael <mael@jousset.com>
2023-11-03 17:04:55 -0300

    download list of cryptos OK, now we need to display it another way

--- src/CryptoListPage.tsx
+++ src/CryptoListPage.tsx
@@ -3,27 +3,55 @@ import CryptoListService from "./CryptoListService";
 
 import {
     Flex,
+    Text,
 } from "@chakra-ui/react"
 import CryptoList from "./CryptoList";
 
+const apiURL = 'https://api.binance.com/api/v3/exchangeInfo';
+
+
+
+
 export const CryptoListPage = () => {
+    const [cryptoData, setCryptoData] = useState([]);
 
-    const cryptoListService = new CryptoListService();
+    async function fetchCryptocurrencies() {
+        try {
+            const response = await fetch(apiURL);
 
-    const [cryptoList, setCryptoList] = useState(cryptoListService.get_crypto_list());
+            if (response.ok) {
+                const data = await response.json();
+                const pairsData = data.symbols;
+                const filteredData = pairsData.filter((s: { symbol: string | string[]; }) => s.symbol.includes('USDT'));
+                const cryptoList = filteredData.map((s: { symbol: any; }) => s.symbol);
+                setCryptoData(cryptoList); // Update state with the fetched data
+            } else {
+                console.error('Failed to fetch data');
+            }
+        } catch (error) {
+            console.error('An error occurred:', error);
+        }
+    }
+
+    // const cryptoListService = new CryptoListService();
+
+    // const [cryptoList, setCryptoList] = useState(cryptoListService.get_crypto_list());
 
     useEffect(() => {
-        const handleCryptoListUpdate = () => {
-            setCryptoList(cryptoListService.get_crypto_list());
-        };
+        fetchCryptocurrencies();
+        // const handleCryptoListUpdate = () => {
+        // setCryptoList(cryptoListService.get_crypto_list());
+        // };
 
-        cryptoListService.registerSendCryptoListCallback(handleCryptoListUpdate);
+        // cryptoListService.registerSendCryptoListCallback(handleCryptoListUpdate);
     }
     );
 
     return (
         <Flex>
-            <CryptoList cryptoList={cryptoList} />
+            {cryptoData.map((crypto, index) => (
+                <Text key={index} color={"white"}>{crypto}</Text>
+            ))}
         </Flex>
     );
 }
